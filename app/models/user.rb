840d64dd4549c38ceef6a94783c1c62a315c2009
hi-maruyaki <imaru0223@gmail.com>
2021-02-19 02:48:47 +0900

    user管理機能

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,10 +5,11 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
          validates :nickname, presence: true
-         validates :last_name, presence: true
-         validates :first_name, presence: true 
-         validates :last_kana, presence: true, format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/  }
-         validates :first_kana, presence: true, format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/  }
+         validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i }
+         validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/ }
+         validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/ }
+         validates :last_kana, presence: true, format: { with: /\A[ァ-ヶー]+\z/ }
+         validates :first_kana, presence: true, format: { with: /\A[ァ-ヶー]+\z/ }
          validates :birth_day, presence: true
 
 end
