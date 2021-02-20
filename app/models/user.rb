d62a1643222e7c25077ebaefe79dea1d7201e710
hi-maruyaki <imaru0223@gmail.com>
2021-02-20 10:25:34 +0900

    upudate

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,12 +4,19 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-         validates :nickname, presence: true
-         validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i }
-         validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/ }
-         validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/ }
-         validates :last_kana, presence: true, format: { with: /\A[ァ-ヶー]+\z/ }
-         validates :first_kana, presence: true, format: { with: /\A[ァ-ヶー]+\z/ }
-         validates :birth_day, presence: true
+        with_options presence: true do
+          validates :nickname
+          validates :birth_day
+          validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i }
+        end
 
+        with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/ }  do
+          validates :last_name
+          validates :first_name
+        end
+
+        with_options presence: true, format: { with: /\A[ァ-ヶー]+\z/ }  do
+          validates :last_kana
+          validates :first_kana
+        end
 end
