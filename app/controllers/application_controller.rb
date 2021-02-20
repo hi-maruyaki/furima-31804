72920022cdac172cff7fc618421a13364abfc719
hi-maruyaki <imaru0223@gmail.com>
2021-02-20 18:15:37 +0900

    ,を除去！

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
 
   private
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :last_name, :first_name, :last_kana,:first_kana,  :birth_day,])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :last_name, :first_name, :last_kana,:first_kana,  :birth_day])
   end
   
 end
