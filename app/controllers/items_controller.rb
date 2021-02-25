d79488824ef21df912fce2e3a7671b8bca6c01e1
hi-maruyaki <imaru0223@gmail.com>
2021-02-25 12:35:35 +0900

    1

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -33,13 +33,11 @@ class ItemsController < ApplicationController
 
 
   def update
-    @item = Item.find(params[:id])
-    @item.update(item_params)
-      if @item.update
-        redirect_to item_path(@item.id)
-      else
-        render :edit
-      end
+    if @item = Item.update(item_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
   end
 
   # def destroy
@@ -48,9 +46,16 @@ class ItemsController < ApplicationController
   #   redirect_to root_path
   # end
 
+
+
   private
 
   def item_params
     params.require(:item).permit(:name, :description, :category_id, :status_id, :carriage_id, :prefecture_id, :days_to_ship_id, :price, :image).merge(user_id: current_user.id)
   end
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
 end
