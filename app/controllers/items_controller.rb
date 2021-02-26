6c676222ab0ba67f7079b807025d7fb8b4ca888d
hi-maruyaki <imaru0223@gmail.com>
2021-02-26 13:43:14 +0900

    1

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :set_item, only: [:show]
-  # before_action :set_item, only: [:edit, :update, :show]
+  before_action :set_item, only: [:edit, :update, :show]
   def index
     @items = Item.all.order(created_at: :desc)
   end
@@ -24,20 +23,20 @@ class ItemsController < ApplicationController
   def show
   end
 
-  # def edit  
-  #   if @item.user_id != current_user.id 
-  #     redirect_to root_path
-  #   end
-  # end
+  def edit  
+    if @item.user_id != current_user.id 
+      redirect_to root_path
+    end
+  end
 
 
-  # def update
-  #   if @item.update(item_params)
-  #     redirect_to root_path
-  #   else
-  #     render :edit
-  #   end
-  # end
+  def update
+    if @item.update(item_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end
 
   # def destroy
   #   item = Item.find(params[:id])
