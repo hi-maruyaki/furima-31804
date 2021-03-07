4f86cda2a021ec7b48c9c5691ec80cde2a185594
hi-maruyaki <imaru0223@gmail.com>
2021-03-07 16:32:36 +0900

    hi!

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,49 @@
+class OrdersController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_item, only: [:index, :create]
+  before_action :move_to_index
+
+  def index
+    @order = UserOrder.new
+    if current_user.id == @item.user.id || @item.order != nil
+      redirect_to root_path
+    end
+  end
+
+  def create
+    @order = UserOrder.new(order_params)
+    if @order.valid?
+      pay_item
+      @order.save
+      redirect_to root_path
+    else
+      render 'index'
+    end
+  end
+
+  private
+
+  def order_params
+    params.permit(:postcode, :prefecture, :city, :address, :building_name, :tellphone,:token, :item_id).merge(user_id: current_user.id)
+  end
+
+
+  def pay_item
+    Payjp.api_key =  "sk_test_0bfb2e13aeabb52921e02cdb" 
+    Payjp::Charge.create(
+      amount: @item.price, # 商品の値段
+      card: order_params[:token], # カードトークン
+      currency: 'jpy' # 通貨の種類（日本円）
+    )
+  end
+
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
+
+  def move_to_index
+    redirect_to root_path unless @item.user_id != current_user.id
+  end
+end
+
+
---
+++ app/models/user_order.rb
@@ -0,0 +1,25 @@
+class UserOrder
+
+  include ActiveModel::Model
+  attr_accessor :postcode, :prefecture, :city, :address, :building_name, :tellphone, :item_id, :user_id, :token
+
+
+  with_options presence: true do
+    validates :postcode,  format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)" }
+    validates :prefecture, numericality: { other_than: 1 }
+    validates :city
+    validates :address
+    validates :tellphone, format: { with: /\A[0][0-9]{10}\z/ }
+    validates :item_id
+    validates :user_id
+    
+  end
+
+
+  def save
+    order = Order.create(item_id: item_id, user_id: user_id)
+    Address.create(postcode: postcode, prefecture: prefecture, city: city, address: address, building_name:  building_name, tellphone: tellphone, order_id: order.id)
+  end
+end
+
+
