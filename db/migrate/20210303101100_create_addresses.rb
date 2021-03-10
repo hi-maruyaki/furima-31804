bd14bebf18e050bc0508079a60b9f0b6637dffab
hi-maruyaki <imaru0223@gmail.com>
2021-03-10 16:47:02 +0900

    0

--- db/migrate/20210303101100_create_addresses.rb
+++ db/migrate/20210303101100_create_addresses.rb
@@ -1,7 +1,7 @@
 class CreateAddresses < ActiveRecord::Migration[6.0]
   def change
     create_table :addresses do |t|
-      t.integer :postcode,     null: false
+      t.string :postcode,     null: false
       t.integer :prefecture,      null: false
       t.string :city,             null: false         
       t.string :address,      null: false
